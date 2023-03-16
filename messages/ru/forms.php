559baf1b8fea0103cf061f5ca30b9e3a9c89f297
BiFiK <git@bifikdev.ru>
2023-03-16 21:36:54 +0300

    - (change) Настроил текстовки;

--- messages/ru/forms.php
+++ messages/ru/forms.php
@@ -1,9 +1,5 @@
 <?php
 
 return [
-    'FORM_USERS_SUCCESS' => 'Вы успешно зарегистрировались в боте',
-    'FORM_USERS_SUCCESS_ACTIVE_1' => 'Вы включили уведомления',
-    'FORM_USERS_SUCCESS_ACTIVE_0' => 'Вы отключили уведомления',
 
-    'ERROR_MESSAGE' => 'Что-то пошло не так. Возможно, что бот сломался',
 ];
--- messages/ru/telegram.php
+++ messages/ru/telegram.php
@@ -1,23 +1,16 @@
 <?php
 
 return [
+    'MESSAGE_TEMPLATE' => "Основная информация
+{active} Уведомления  
+{ready} Готовность
+ 
+Статистика:
+{iconSmoke} Общее кол-во перекуров: {smokeRequestAll}
+{iconSmoke} Участвовал в перекурах: {smokeRequestWithMe} 
+{iconTimer} Последний перекур: {lastSmokeRequest}",
+
     'MESSAGE_ERROR' => 'Тебя нет в боте. Используй /start',
-    'MESSAGE_START_INBASE' => 'Ты уже зарегистрирован в боте',
-
-    'MESSAGE_ACTIVE_1' => 'Меняешь активность ?',
-    'MESSAGE_ACTIVE_2_0' => 'Ты вновь начал курить ?',
-    'MESSAGE_ACTIVE_2_1' => 'Неужели бросил курить ?',
-    'MESSAGE_ACTIVE_3' => 'Ладно. Шучу',
-    'MESSAGE_ACTIVE_4' => 'Пытаюсь обновить активность',
-
-    'MESSAGE_INFO_CHECK' => 'Проверяю ',
-
-    'MESSAGE_SMOKE_1' => 'Ты хочешь покурить ?',
-    'MESSAGE_SMOKE_2' => 'Пытаюсь найти с кем покурить',
-    'MESSAGE_SMOKE_3' => 'Рассылаю уведомления',
-
-    'MESSAGE_START_1' => 'Ты новенький ?',
-    'MESSAGE_START_2' => 'Пытаюсь сохранить информацию о тебе',
-    'MESSAGE_START_3' => 'Пытаюсь подписать на уведомления',
+    'MESSAGE_START' => 'Регистрация в боте',
 
 ];
